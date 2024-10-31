3018cdcc0d975f98081701a2111fa8007031c96d
Michael Schmidt <m.schmidt@unsw.edu.au>
2024-10-31 16:37:06 +1100

    added heavy quark contribution to scalar interactions through gluon operator

--- flavio/physics/mudecays/mueconversion.py
+++ flavio/physics/mudecays/mueconversion.py
@@ -24,8 +24,35 @@ def CR_mue(wc_obj, par, nucl):
     omega_capt  = par['GammaCapture '+nucl]
     #####Wilson Coefficients######
     #####Conversion Rate obtained from hep-ph/0203110#####
+    ### note all operators are conjugated with respect to hep-ph/0203110, e.g. (-4 m_mu G_F/\sqrt{2}) A_L mubar sigma^{mu nu } P_R e F_{\mu\nu}###
+    ### It is consistently implemented. ###
     flavio.citations.register("Kitano:2002mt")
+    wc = wc_obj.get_wc('mue', par['m_b'], par, nf_out=5)
+    wcc = wc_obj.get_wc('mue', par['m_c'], par, nf_out=4)
+    wct=wc_obj.match_run(scale=160,eft='SMEFT',basis='Warsaw')
+
+    gRSheavy ={
+        'b': prefac * ( wc['CSRR_emubb'] + wc['CSRL_emubb'] ).conjugate() / 2,
+        'c': prefac * ( wcc['CSRR_emucc'] + wcc['CSRL_emucc'] ).conjugate() / 2,
+        't': prefac * ( -wct['lequ1_1233'] ).conjugate() / 2,
+    } 
+    gLSheavy ={
+        'b': prefac * ( wc['CSRR_muebb'] + wc['CSRL_muebb'] ) / 2,
+        'c': prefac * ( wcc['CSRR_muecc'] + wcc['CSRL_muecc'] ) / 2,
+        't': prefac * ( - wct['lequ1_2133'] ) / 2,
+    }
+    mu = flavio.physics.running.running.get_mu(par, scale, nf_out=3)
+    md = flavio.physics.running.running.get_md(par, scale, nf_out=3)
+    ms = flavio.physics.running.running.get_ms(par, scale, nf_out=3)
+
+    mc = par['m_c']
+    mb = par['m_b']
+    mt = par['m_t']
+    mp = par['m_p']
+    mn = par['m_n']
     wc = wc_obj.get_wc('mue', scale, par, nf_out=3)
+    GGpfac = -(mp - mu*GuSp - md* GdSp - ms* GsSp )/(12*np.pi) 
+    GGnfac = -(mn - mu*GuSn - md* GdSn - ms* GsSn )/(12*np.pi) 
     prefac = -np.sqrt(2)/par['GF']
     AL = prefac / ( 4 * mm ) * wc['Cgamma_emu']
     AR = prefac / ( 4 * mm ) * wc['Cgamma_mue'].conjugate()
@@ -53,15 +80,15 @@ def CR_mue(wc_obj, par, nucl):
         AR.conjugate() * D
         + ( 2 * gLV['u'] + gLV['d'] ) * Vp
         + ( gLV['u'] + 2 * gLV['d'] ) * Vn
-        + ( GuSp * gLS['u'] + GdSp * gLS['d'] + GsSp * gLS['s'] ) * Sp
-        + ( GuSn * gLS['u'] + GdSn * gLS['d'] + GsSn * gLS['s'] ) * Sn
+        + ( GuSp * gLS['u'] + GdSp * gLS['d'] + GsSp * gLS['s'] + GGpfac (gLSheavy['b']/mb + gLSheavy['c']/mc + gLSheavy['t']/mt )) * Sp
+        + ( GuSn * gLS['u'] + GdSn * gLS['d'] + GsSn * gLS['s'] + GGnfac (gLSheavy['b']/mb + gLSheavy['c']/mc + gLSheavy['t']/mt )) * Sn
     )
     rhc = (
         AL.conjugate() * D
         + ( 2 * gRV['u'] + gRV['d'] ) * Vp
         + ( gRV['u'] + 2 * gRV['d'] ) * Vn
-        + ( GuSp * gRS['u'] + GdSp * gRS['d'] + GsSp * gRS['s'] ) * Sp
-        + ( GuSn * gRS['u'] + GdSn * gRS['d'] + GsSn * gRS['s'] ) * Sn
+        + ( GuSp * gRS['u'] + GdSp * gRS['d'] + GsSp * gRS['s'] + GGpfac (gRSheavy['b']/mb + gRSheavy['c']/mc + gLSheavy['t']/mt )) * Sp
+        + ( GuSn * gRS['u'] + GdSn * gRS['d'] + GsSn * gRS['s'] + GGnfac (gRSheavy['b']/mb + gRSheavy['c']/mc + gLSheavy['t']/mt )) * Sn
     )
     omega_conv = 2 * par['GF']**2 * ( abs(lhc)**2 + abs(rhc)**2 )
     return omega_conv / omega_capt
